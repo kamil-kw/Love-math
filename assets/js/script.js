9830942683b91e984fdd41ef9539312c9306a408
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-08-08 17:58:15 +0000

    Add favicon. and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,12 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         })
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key == "Enter") {
+            checkAnswer();
+        }
+    })
+    
     runGame("addition");
 
 })
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function(){
 */
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //creates two randome numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
