11fe02807ad13b8d29430de2aead5d73b9b0f5cb
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2021-08-08 13:27:56 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+
+function calculateCorrecAnswer(){
+
+}
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubstractQuestion (){
+
+}
+
+function displayMultipyQuestion(){
+
+}
